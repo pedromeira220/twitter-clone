75793d5533053c9914875be2987bf90b8dc90296
Pedro Meira <petergamer8k@gmail.com>
2022-08-12 17:39:01 -0300

    Changing the name of the endpoint user/create to user/register

--- server/src/router.ts
+++ server/src/router.ts
@@ -3,7 +3,7 @@ import { createUserController } from "./useCases/createUser";
 
 const router = Router();
 
-router.post("/user/create", (req, res) => {
+router.post("/user/register", (req, res) => {
 	return createUserController.handle(req, res);
 });
 
